51ec17011f51a4ed4d897d3287a15b5af4c2a4dc
juanluna96 <juandluna17@gmail.com>
2021-02-18 22:17:52 -0500

    Creando el State y Leyendo lo que el usuario selecciona

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Formulario = () => {
+
+    // State del formulario
+    const [busqueda, setBusqueda] = useState({
+        ciudad: '',
+        pais: ''
+    });
+
+    // Extraer ciudad y pais
+    const { ciudad, pais } = busqueda;
+
+    // Funcion que coloca los elementos en el state
+    const handleChange = (e) => {
+        // actualizar el state
+        setBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <form action="">
             <div className="input-field col s12">
-                <input type="text" name="ciudad" id="ciudad" className="validate" />
+                <input type="text" name="ciudad" id="ciudad" className="validate" value={ ciudad } onChange={ handleChange } />
                 <label htmlFor="ciudad">Ciudad:</label>
             </div>
             <div className="input-field col s12">
-                <select name="pais" id="pais">
+                <select name="pais" id="pais" value={ pais } onChange={ handleChange }>
                     <option value="">--Seleccione un pais--</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="pais">País:</label>
             </div>
