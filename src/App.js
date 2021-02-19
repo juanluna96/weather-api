06421c806153e14f16c484d86b5ac2452ceba66f
juanluna96 <juandluna17@gmail.com>
2021-02-18 22:02:04 -0500

    Creando el Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Clima react</h1>
+    <Fragment>
+      <Header titulo="Clima react app"></Header>
+    </Fragment >
   );
 }
 
